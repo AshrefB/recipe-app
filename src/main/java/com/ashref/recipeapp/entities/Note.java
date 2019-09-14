9137f4d8828c81f9c8caabf6d3a09be485ebafec
Achref Bouadila <a_bouadila@live.fr>
2019-09-14 02:33:06 +0100

    Added Recipe and Note Entities with OneToOne relationship. Closes #1

---
+++ src/main/java/com/ashref/recipeapp/entities/Note.java
@@ -0,0 +1,38 @@
+package com.ashref.recipeapp.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    private String content;
+    @OneToOne
+    private Recipe recipe;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
