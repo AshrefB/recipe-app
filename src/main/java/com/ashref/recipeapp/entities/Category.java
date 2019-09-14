5ecb2c28cd639afb83338868884a742ea8bc157b
Achref Bouadila <a_bouadila@live.fr>
2019-09-14 03:54:34 +0100

    Added Category Entity and ManyToMany relationship with recipe. Closes #5

---
+++ src/main/java/com/ashref/recipeapp/entities/Category.java
@@ -0,0 +1,39 @@
+package com.ashref.recipeapp.entities;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
--- src/main/java/com/ashref/recipeapp/entities/Recipe.java
+++ src/main/java/com/ashref/recipeapp/entities/Recipe.java
@@ -24,6 +24,13 @@ public class Recipe {
     private Set<Ingredient> ingredients = new HashSet<>();
     @OneToOne(cascade = CascadeType.ALL)
     private Note note;
+    @ManyToMany
+    @JoinTable(
+            name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
