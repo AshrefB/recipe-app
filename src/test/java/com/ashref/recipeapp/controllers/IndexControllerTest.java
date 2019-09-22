bf7179669b3ebf85cd365517888eb763dcfb8eea
Achref Bouadila <a_bouadila@live.fr>
2019-09-22 22:02:43 +0100

    Enhanced IndexController Unit Test with ArgumentCaptor. Closes #18

--- src/test/java/com/ashref/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/ashref/recipeapp/controllers/IndexControllerTest.java
@@ -1,17 +1,20 @@
 package com.ashref.recipeapp.controllers;
 
+import com.ashref.recipeapp.entities.Recipe;
 import com.ashref.recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
     private IndexController indexController;
@@ -28,8 +31,20 @@ public class IndexControllerTest {
 
     @Test
     public void index() {
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        recipes.add(recipe1);
+        Recipe recipe2 = new Recipe();
+        recipe1.setId(2L);
+        recipes.add(recipe2);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
         assertEquals("index", indexController.index(model));
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        assertEquals(2, argumentCaptor.getValue().size());
     }
 }
