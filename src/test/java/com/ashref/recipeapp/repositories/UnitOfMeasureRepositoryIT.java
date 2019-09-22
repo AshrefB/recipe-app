f28169beccf88b7e6ea4a907ed4f0e8e3570ea0a
Achref Bouadila <a_bouadila@live.fr>
2019-09-22 23:27:35 +0100

    Added Integration Test for UnitOfMeasureRepository. Closes #20

---
+++ src/test/java/com/ashref/recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,31 @@
+package com.ashref.recipeapp.repositories;
+
+import com.ashref.recipeapp.entities.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureRepositoryIT {
+    @Autowired
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findByDescription() {
+        Optional<UnitOfMeasure> uom = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        assertEquals("Teaspoon", uom.get().getDescription());
+    }
+}
